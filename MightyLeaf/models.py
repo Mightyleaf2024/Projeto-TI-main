9d26075a3ef1021af7b42f62f8945426438140cb
P4t0P06 <160739849+P4t0P06@users.noreply.github.com>
2024-11-21 09:18:37 -0300

    alteração class usuario

--- MightyLeaf/models.py
+++ MightyLeaf/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Cliente(models.Model):
-    id = models.IPAddressField
     nome = models.CharField('Nome do Cliente', max_length=100)
     data_nascimeto = models.DateField('Data nascimento',blank=False)
     email = models.EmailField('E-mail',max_length=100,blank=False, unique = True)
