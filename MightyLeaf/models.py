6489852db9e48e00af93741fc0ebb35283986754
P4t0P06 <160739849+P4t0P06@users.noreply.github.com>
2024-11-21 09:13:46 -0300

    56

--- MightyLeaf/models.py
+++ MightyLeaf/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class Cliente(models.Model):
-    cpf = models.BigIntegerField('CPF', primary_key=True, unique=True)
+    id = models.IPAddressField
     nome = models.CharField('Nome do Cliente', max_length=100)
     data_nascimeto = models.DateField('Data nascimento',blank=False)
     email = models.EmailField('E-mail',max_length=100,blank=False, unique = True)
-    telefonte = models.BigIntegerField('telefone',blank=False,help_text='inserir numero com DDD')
 
     def __str__(self):
         return self.cpf
