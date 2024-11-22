40999b93d43d4c24d84c20e633275bd2f7749470
Mightyleaf2024 <mightyleaf24@gmail.com>
2024-11-22 11:34:24 -0300

    2

--- MightyLeaf/forms.py
+++ MightyLeaf/forms.py
@@ -1,8 +1,1 @@
 from django import forms        
-
-from .models import Cliente
-
-class ClienteModelForm(forms.ModelsForm):
-    class Meta:
-        model = Cliente
-        fields = ['cpf','nome','date_nascimento','email', 'telefone']
